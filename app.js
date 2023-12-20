f415d2eac3d26b46adf1b9c49eeab54522a6f68c
richardzk17 <richardhernandezk17@gmail.com>
2023-12-19 21:11:08 -0500

    Created shuffle, deck function

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("check")
+const suits = ['Clubs', 'Diamonds','Spades', 'Hearts']
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ]
+const deck = []
+
+createDeck()
+shuffle(deck)
+
+
+function createDeck() {
+    suits.forEach(suit => {
+        values.forEach(value => {
+            deck.push(`${suit} ${value}`)
+        })
+    })
+}
+
+function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        let randomCard = Math.floor(Math.random() * deck.length)
+        let temporaryCard = deck[i]
+        deck[i] = deck[randomCard]
+        deck[randomCard] = temporaryCard
+    }
+}
+
+console.log(deck)
